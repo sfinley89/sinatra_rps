0a80e161005f65ee6ccfa9ead0b34be7a4c92843
sfinley89 <sfinley@hovelstay.com>
2015-10-03 15:43:00 -0700

    works

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,14 @@
-# myapp.rb
 require 'sinatra'
 
+# Renders the html file views/index.erb when you go to http://localhost:4567/
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/rps' do
+    @weapons = ["rock", "paper", "scissors"]
+    @cmp_choice = @weapons.shuffle[0]
+    @choice = params[:choice]  
+
+    erb :rps
 end
---
+++ routes.rb
@@ -0,0 +1,38 @@
+get '/' do
+  .. show something ..
+end
+
+post '/' do
+  .. create something ..
+end
+
+put '/' do
+  .. replace something ..
+end
+
+patch '/' do
+  .. modify something ..
+end
+
+delete '/' do
+  .. annihilate something ..
+end
+
+options '/' do
+  .. appease something ..
+end
+
+link '/' do
+  .. affiliate something ..
+end
+
+unlink '/' do
+  .. separate something ..
+end
+
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
+end
